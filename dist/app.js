f027de18aff2758c9b320537f0e5150823835630
Ginger Williams <gingerwilliams@MacBook-Pro-2.local>
2021-06-22 18:08:58 -0400

    set up postgresql db connection

--- dist/app.js
+++ dist/app.js
@@ -3,15 +3,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+var pg_pool_1 = __importDefault(require("pg-pool"));
 var express_1 = __importDefault(require("express"));
 var cors_1 = __importDefault(require("cors"));
+var pgConfig_1 = __importDefault(require("./pgConfig"));
 var PORT = "3000";
 var reactUi = "8000";
 // const nx = "4200";
 var app = express_1.default();
+var pool = new pg_pool_1.default(pgConfig_1.default);
+pool
+    .connect()
+    .then(function (data) { return console.log("hello im connected:", data); })
+    .catch(function (error) { return console.log(error); });
 app.use(cors_1.default({
     origin: ["http://localhost:" + reactUi]
 }));
+app.get('/', function (request, response) {
+    response.json({ info: 'Node.js, Express, and Postgres API' });
+});
 app.listen(PORT, function () {
     console.log("hello microservices: listening to " + PORT);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
+import Pool from "pg-pool";
 import express from "express";
 import cors from "cors";
+import config from "./pgConfig";
 
 const PORT = "3000";
 const reactUi = "8000";
@@ -7,9 +9,20 @@ const reactUi = "8000";
 
 const app = express();
 
+const pool = new Pool(config);
+
+pool
+    .connect()
+    .then(data => console.log("hello im connected:", data))
+    .catch(error => console.log(error))
+
 app.use(cors({
     origin: [`http://localhost:${reactUi}`]
 }));
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+  })
 
 app.listen(PORT, () => {
     console.log(`hello microservices: listening to ${PORT}`);
